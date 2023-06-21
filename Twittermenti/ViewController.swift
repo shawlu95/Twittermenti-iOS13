4f5d23d64b9512285940b8325af25cfa00a1caa3
Shaw Lu <shawlu95@gmail.com>
2023-06-20 22:26:01 -0700

    search tweets using API
    See https://peterfriese.dev/posts/reading-api-keys-from-plist-files/

--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -7,16 +7,49 @@
 //
 
 import UIKit
+import SwifteriOS
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var sentimentLabel: UILabel!
+    
+    private var apiKey: String {
+      get {
+        guard let filePath = Bundle.main.path(forResource: "Secrets", ofType: "plist") else {
+          fatalError("Couldn't find file 'Secrets.plist'.")
+        }
+        let plist = NSDictionary(contentsOfFile: filePath)
+        guard let value = plist?.object(forKey: "API_KEY") as? String else {
+          fatalError("Couldn't find key 'API_KEY' in 'Secrets.plist'.")
+        }
+        return value
+      }
+    }
+    
+    private var apiSecret: String {
+      get {
+        guard let filePath = Bundle.main.path(forResource: "Secrets", ofType: "plist") else {
+          fatalError("Couldn't find file 'Secrets.plist'.")
+        }
+        let plist = NSDictionary(contentsOfFile: filePath)
+        guard let value = plist?.object(forKey: "API_SECRET") as? String else {
+          fatalError("Couldn't find key 'API_SECRET' in 'Secrets.plist'.")
+        }
+        return value
+      }
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        let swifter = Swifter(consumerKey: apiKey, consumerSecret: apiSecret)
+        swifter.searchTweet(using: "@Apple", lang: "en", count: 100, tweetMode: .extended, success: { (results, metadata) in
+            print(results)
+        }) { (error) in
+            print("There was an error with the Twitter API Request, \(error)")
+        }
     }
 
     @IBAction func predictPressed(_ sender: Any) {
